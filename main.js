847d4d03579595b0f48c161f059ea75399ff0ac2
Thomas1301 <duarte.thomas1301@gmail.com>
2023-05-14 20:33:37 +0200

    Grossisement des titres, ajout d'une image principale et ajout de la date du message envoyé

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const client = new discord.Client({ intents });
 const loadCommands = require("./Loader/loadCommands");
 const loadEvents = require("./Loader/loadEvents");
 const { token } = require("./config.json");
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 client.commands = new discord.Collection();
 
@@ -42,35 +42,43 @@ const fetchTopBassHousePlaylist = async () => {
   const sortedTracks = bassHousePlaylist.tracks.items.sort(
     (a, b) => b.popularity - a.popularity
   );
-
   const formatTrackList = (trackList) => {
-    const formattedList = trackList.map((track, index) => {
-      const artists = track.track.artists.map((artist) => artist.name).join(", ");
-      const imageUrl = track.track.album.images[0].url; // Ajouter l'URL de l'image
-      return `${index + 1}. [${track.track.name}](${track.track.external_urls.spotify}) - ${artists} [![image](${imageUrl})]`;
-    }).join("\n");
+    const formattedList = trackList
+      .map((track, index) => {
+        const artists = track.track.artists
+          .map((artist) => artist.name)
+          .join(", ");
+        const imageUrl = track.track.album.images[0].url; // Ajouter l'URL de l'image
+        return `${index + 1}. **[${track.track.name}](${
+          track.track.external_urls.spotify
+        })** - ${artists} [[Track image here](${imageUrl})]`;
+      })
+      .join("\n");
     return formattedList;
   };
 
   // Get the top 10 tracks by popularity
-  const top10TracksFormated = formatTrackList(sortedTracks.slice(0, 11));
-// Format the list of tracks
+  const top10TracksFormated = await formatTrackList(sortedTracks.slice(0, 11));
+  // Format the list of tracks
   const bassHouseChannel = client.channels.cache.find(
     (channel) => channel.name === "bass-house"
   );
   if (!bassHouseChannel) return console.error("Channel not found");
-  
-   // Create the message embed
-   const embed = new EmbedBuilder()
-   .setColor('#1DB954')
-   .setTitle('Top 10 Bass House Songs on Spotify')
-   .setDescription(top10TracksFormated)
-   // Send the message
-   bassHouseChannel.send({ embeds: [embed] });
 
+  const firstTrackPicture = sortedTracks[0].track.album.images[0].url;
+  // Create the message embed
+  const embed = new EmbedBuilder()
+    .setColor("#1DB954")
+    .setTitle("Top 10 Bass House Songs on Spotify")
+    .setDescription(top10TracksFormated)
+    .setImage(firstTrackPicture)
+    .setTimestamp();
+
+  // Send the message
+  bassHouseChannel.send({ embeds: [embed] });
 };
 // Envoyer le top 10 une fois par jour
-const intervalMs = 10 * 1000; // 1 minute
+const intervalMs = 5 * 1000; // 4 secondes
 //const intervalMs = 24 * 60 * 60 * 1000; // 24 heures
 setInterval(fetchTopBassHousePlaylist, intervalMs);
 //fetchTopBassHousePlaylist();
